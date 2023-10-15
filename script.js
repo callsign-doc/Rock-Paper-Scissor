023b67deb13ee97f637c3839c2179d24b742d5b0
Iman Haqim <imanhqm@protonmail.com>
2023-10-15 20:15:13 +0800

    Fix game logic and playerChoice not updating

--- script.js
+++ script.js
@@ -1,46 +1,49 @@
 // declare variable
-let computerChoice = ['Rock', 'Paper', 'Scissor'];
+let computerChoice;
 // player pick choice
-let playerChoice = prompt("Please pick your choice.") 
+let playerChoice = prompt("Please pick your choice.");
 let isInputValid;
 
-//computer make choice
+//DEFINE: computer make choice
 function getComputerChoice() {
-    let randomNumber = Math.floor(Math.random() * 4);
+    let randomNumber = Math.floor(Math.random() * 3);
     console.log(randomNumber);
 
-    if (randomNumber == 0 || randomNumber == 1) {
+    if (randomNumber == 0) {
         computerChoice = 'Rock';
-    } else if (randomNumber == 2) {
+    } else if (randomNumber == 1) {
         computerChoice = 'Paper';
     } else {
-        computerChoice = 'Scissor';
+        computerChoice = 'Scissors';
     }
     console.log(computerChoice);
 }
 
 // DEFINE FUNCTION: check input validity of player
-function checkInputValidity(playerChoice) {
-    if (typeof playerChoice === 'string' ) {
-        let firstChar = playerChoice.charAt(0).toUpperCase();
-        let charAfterZero = playerChoice.slice(1).toLowerCase();
+function checkInputValidity(x) {
+    if (typeof x === 'string' ) {
+        let firstChar = x.charAt(0).toUpperCase();
+        let charAfterZero = x.slice(1).toLowerCase();
 
         playerChoice = firstChar + charAfterZero;
 
-        if (playerChoice === 'Rock' || playerChoice === 'Paper' || playerChoice == "Scissor") {
+        if (playerChoice === 'Rock' || playerChoice === 'Paper' || playerChoice == "Scissors") {
             isInputValid = true;
         } else {
             isInputValid = false;
             console.log('Player input is valid');
         }
-    };
-};
+    }
+}
 
 //function to start round
 function playRound(playerChoice, computerChoice) {
-    checkInputValidity(playerChoice);
+    // states both parties choice
+    console.log("Player choice is " + playerChoice);
+    console.log("Computer choice is " + computerChoice);
 
-    if (isInputValid) {
+    // check who wins
+    if (isInputValid) {  
         switch (playerChoice) {
             case "Rock":
               switch (computerChoice) {
@@ -87,26 +90,26 @@ function playRound(playerChoice, computerChoice) {
             default:
               console.log("Invalid choice. Please choose 'Rock', 'Paper', or 'Scissors'.");
               break;
-        }
+          }   
 
 
 
         /*
-        if (playerChoice === 'Rock' && computerChoice)
+        if (playerChoice === 'Rock') {
+            console.log('This ROCKS yo')
+        }
         console.log('input is valid yo');
 
     } else {
         console.log('input INVALID YO!');
         */
     }
-
-    console.log("Player choice is " + playerChoice);
-    console.log("Computer choice is " + computerChoice);
 };
 
 
 // START ROUND
 getComputerChoice();
+checkInputValidity(playerChoice);
 playRound(playerChoice, computerChoice);
 
 
